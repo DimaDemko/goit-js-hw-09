1cbe493b27c9a6a88191f20506c9249a6cfe595b
DimaDemko <idmitriy4g@gmail.com>
2022-08-10 01:27:23 +0300

    firstTask

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const start = document.querySelector('[data-start]');
+const stopp = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function bgcolour() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+start.addEventListener('click', () => {
+  timerStartId = setInterval(bgcolour, 1000);
+  start.setAttribute('disabled', 'disable');
+  stopp.removeAttribute('disabled', 'disable');
+});
+
+stopp.addEventListener('click', () => {
+  clearInterval(timerStartId);
+  start.removeAttribute('disabled', 'disable');
+  stopp.setAttribute('disabled', 'disable');
+});

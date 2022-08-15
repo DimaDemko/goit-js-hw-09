ab37be3864ba3cae0ab465fc08196c14f657e344
DimaDemko <idmitriy4g@gmail.com>
2022-08-15 16:11:10 +0300

    taskCompleted

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -13,11 +13,21 @@ function bgcolour() {
 start.addEventListener('click', () => {
   timerStartId = setInterval(bgcolour, 1000);
   start.setAttribute('disabled', 'disable');
-  stopp.removeAttribute('disabled', 'disable');
+  stopp.removeAttribute('disabled');
 });
 
 stopp.addEventListener('click', () => {
   clearInterval(timerStartId);
-  start.removeAttribute('disabled', 'disable');
+  start.removeAttribute('disabled');
   stopp.setAttribute('disabled', 'disable');
 });
+
+function isIsogram(str) {
+  const lowerCaseString = str.toLowerCase();
+  let string = '';
+  for (i = str.length - 1; i >= 0; i -= 1) {
+    string += str[i];
+  }
+  console.log(string);
+}
+console.log(isIsogram('wordisbig'));
